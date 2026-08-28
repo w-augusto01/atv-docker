0cda01002c7fcfa2caa724fdec7d35d55bb7748c
w-augusto01 <wesleifernandoaugusto@gmail.com>
2026-08-28 01:27:30 -0400

    corrige acesso do node ao mysql

--- scripts/03-criar-node.ps1
+++ scripts/03-criar-node.ps1
@@ -1,17 +1,13 @@
-docker build -t atv-node .
-
-docker volume create node_data
-
 docker run -d `
     --name node-app `
     --network rede_atividade `
     -p 3000:3000 `
-    --memory=128m `
-    --cpus=0.2 `
     -e DB_HOST=mysql-db `
-    -e DB_USER=root `
+    -e DB_USER=devops `
     -e DB_PASSWORD=devops `
     -e DB_DATABASE=loja `
     -e DB_PORT=3306 `
+    --memory=128m `
+    --cpus=0.2 `
     -v node_data:/app/data `
     atv-node
